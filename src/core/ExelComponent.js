daec955f6164a0766624cfc92ffb3a77a5474cf6
Alex <sheph@ukr.net>
2026-01-06 20:43:27 +0100

    prepared a structure for events

--- src/core/ExelComponent.js
+++ src/core/ExelComponent.js
@@ -1,7 +1,14 @@
 import { DomListener } from '@core/DomListener';
 export class ExelComponent extends DomListener {
+	constructor($root, options = {}) {
+		super($root, options.listeners);
+	}
 	// Возвращает шаблон компонента
 	toHTML() {
 		return '';
+	}
+
+	init() {
+		this.initDOMListeners();
 	}
 }
